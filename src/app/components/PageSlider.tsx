e6728e5b08ec50775ec814e6b7183b56f55828fe
Elemo Of Lagos <59938157+Elemo9@users.noreply.github.com>
2025-02-12 12:40:12 +0100

    Update PageSlider.tsx

--- src/app/components/PageSlider.tsx
+++ src/app/components/PageSlider.tsx
@@ -1,66 +1,62 @@
 "use client";
-import React,{useRef, useState} from 'react'
-import {Swiper, SwiperSlide} from "swiper/react";
+
+import React from "react";
+import Image from "next/image"; // ✅ Import Next.js Image
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper Styles
-import "swiper/css"
-import "swiper/css/effect-coverflow"
-import "swiper/css/pagination"
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 
-import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
+import { EffectCoverflow, Autoplay } from "swiper/modules"; // ✅ Removed Unused Pagination
 
 function PageSlider() {
   return (
-    <section className='w-full h-fit py-24 flex items-center justify-center'>
-        <Swiper
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={"auto"}
-            loop={true}
-            autoplay={{
-                delay:3500,
-                disableOnInteraction:false
-            }}
-            coverflowEffect={{
-                rotate: 45,
-                stretch:0,
-                depth:80,
-                modifier:1,
-                slideShadows:true
-            }}
-            modules={[EffectCoverflow, Autoplay]}
-            className='mySwiper'
-        >
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242206/IMG_4059_bh2tas.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242202/IMG_4031_sjvzml.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242201/IMG_4074_knenq4.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242197/IMG_3799_hv6san.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242182/IMG_3394_zjpymq.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242182/IMG_3267_v9hnjy.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242181/IMG_2850_lptbrl.jpg"} alt=''/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={"https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242180/IMG_2272-1_mhlena.jpg"} alt=''/>
-            </SwiperSlide>
-        </Swiper>
-
-
+    <section className="w-full h-fit py-24 flex items-center justify-center">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        loop={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        coverflowEffect={{
+          rotate: 45,
+          stretch: 0,
+          depth: 80,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow, Autoplay]}
+        className="mySwiper"
+      >
+        {[
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242206/IMG_4059_bh2tas.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242202/IMG_4031_sjvzml.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242201/IMG_4074_knenq4.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242197/IMG_3799_hv6san.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242182/IMG_3394_zjpymq.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242182/IMG_3267_v9hnjy.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242181/IMG_2850_lptbrl.jpg",
+          "https://res.cloudinary.com/dyn1k6wcl/image/upload/v1739242180/IMG_2272-1_mhlena.jpg",
+        ].map((src, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={src}
+              alt={`Fashion Slide ${index + 1}`}
+              width={600}
+              height={800}
+              className="w-full h-auto object-cover rounded-xl"
+              priority={index === 0} // First image loads first for better LCP
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
-  )
+  );
 }
 
-export default PageSlider
+export default PageSlider;
