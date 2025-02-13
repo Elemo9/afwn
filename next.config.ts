93e015291bef7c17e3ef944da1a4e753e0d64f84
Elemo Of Lagos <59938157+Elemo9@users.noreply.github.com>
2025-02-14 00:03:26 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
